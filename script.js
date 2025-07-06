86f6fe921c3658fbd7d2808af21a5d78abe30820
sulyman002 <oyedelesulaiman@gmail.com>
2025-07-06 21:36:35 +0100

    almost done with home page

---
+++ script.js
@@ -0,0 +1,19 @@
+const icon_hamburger = document.querySelector('.icon_hamburger');
+const closeNav = document.querySelector('.closeNav');
+const navContent = document.querySelector('.navContent');
+
+icon_hamburger.addEventListener('click', () => {
+    navContent.style.display = 'flex' 
+
+});
+
+closeNav.addEventListener('click', () => {
+    navContent.style.display = 'none' 
+
+});
+
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+
+    });
