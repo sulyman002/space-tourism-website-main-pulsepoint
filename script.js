1bf57bfc73cdb19c01162afa533962ce8cec7301
sulyman002 <oyedelesulaiman@gmail.com>
2025-07-07 16:28:45 +0100

    two page left

--- script.js
+++ script.js
@@ -3,18 +3,83 @@ const closeNav = document.querySelector('.closeNav');
 const navContent = document.querySelector('.navContent');
 
 icon_hamburger.addEventListener('click', () => {
-    navContent.style.display = 'flex' 
+    navContent.style.display = 'flex'
 
 });
 
 closeNav.addEventListener('click', () => {
     navContent.style.display = 'none'
-    location.reload(); 
+
 
 });
 
-fetch('data.json')
+fetch('starter-code/data.json')
     .then(response => response.json())
     .then(data => {
+
+        const destination = data.destinations;
+        
+        const moon = document.querySelector('.moon');
+        const mars = document.querySelector('.mars');
+        const europa = document.querySelector('.europa');
+        const titan = document.querySelector('.titan');
+
+        moon.addEventListener('click', () => {
+            destination.forEach((destination, index) => {
+                if(index === 0) {
+                    document.querySelector('.destinationName').innerHTML = destination.name.toUpperCase();
+                    document.querySelector('.destinationDetails').innerHTML = destination.description;
+                    document.querySelector('.destinationAvg').innerHTML = destination.distance;
+                    document.querySelector('.destinationTravelTime').innerHTML = destination.travel;
+                    document.querySelector('.moonImg').scr = destination.images.png
+
+                }
+                
+            });
+        });
+
+        mars.addEventListener('click', () => {
+            destination.forEach((destination, index) => {
+                if(index === 1) {
+                    document.querySelector('.destinationName').innerHTML = destination.name.toUpperCase();
+                    document.querySelector('.destinationDetails').innerHTML = destination.description;
+                    document.querySelector('.destinationAvg').innerHTML = destination.distance;
+                    document.querySelector('.destinationTravelTime').innerHTML = destination.travel;
+                    document.querySelector('.moonImg').scr = destination.images.png
+
+                }
+                
+            });
+        });
+
+        europa.addEventListener('click', () => {
+            destination.forEach((destination, index) => {
+                if(index === 2) {
+                    document.querySelector('.destinationName').innerHTML = destination.name.toUpperCase();
+                    document.querySelector('.destinationDetails').innerHTML = destination.description;
+                    document.querySelector('.destinationAvg').innerHTML = destination.distance;
+                    document.querySelector('.destinationTravelTime').innerHTML = destination.travel;
+                    document.querySelector('.moonImg').scr = destination.images.png
+
+                }
+                
+            });
+        });
+
+        titan.addEventListener('click', () => {
+            destination.forEach((destination, index) => {
+                if(index === 3) {
+                    document.querySelector('.destinationName').innerHTML = destination.name.toUpperCase();
+                    document.querySelector('.destinationDetails').innerHTML = destination.description;
+                    document.querySelector('.destinationAvg').innerHTML = destination.distance;
+                    document.querySelector('.destinationTravelTime').innerHTML = destination.travel;
+                    document.querySelector('.moonImg').scr = destination.images.png
+
+                }
+                
+            });
+        });
+
+
 
     });
